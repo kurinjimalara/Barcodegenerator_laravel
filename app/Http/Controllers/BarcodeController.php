dc889a18248850d7bba118e1125840f7070897d8
Kurinjimalar A <kurinjimalar.a@optisolbusiness.com>
2023-08-11 13:51:43 +0530

    Task

--- app/Http/Controllers/BarcodeController.php
+++ app/Http/Controllers/BarcodeController.php
@@ -9,17 +9,20 @@ use Illuminate\Support\Facades\Storage;
 
 class BarcodeController extends Controller
 {
-    // save barcode image
+    // This function is used to generate 2D Barcode 
     function post_data(Request $request)
     {
 
-        $code = $request->productcode;
+        $data = $request->productcode;
         $type = $request->type;
-        $qrCode = \DNS1D::getBarcodePNG( $code,$type );
-        $path= \Storage::disk('public')->put(time().'.png',base64_decode(\DNS1D::getBarcodePNG($code,$type )));
+        // Here we have created barcode based on barcode data and barcode type
+        $qrCode = \DNS1D::getBarcodePNG( $data,$type );
+        // Here we have stored the image of created barcode in storage->app->public folder
+        $storage_status= \Storage::disk('public')->put(time().'.png',base64_decode(\DNS1D::getBarcodePNG($data,$type )));
         $hostname = env("APP_URL"); 
+        // Here we have created storage path link for created barcode image
         $st_path=$hostname.'/milon_final/milon_barcode/storage/app/public/'.time().'.png';
-          return ['productcode'=>$code,'type'=>$type,'st_path'=>$st_path];
+          return ['productcode'=>$data,'type'=>$type,'storage_status'=>$storage_status,'st_path'=>$st_path];
           
     }
    
